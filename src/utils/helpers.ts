413d16000e5a06a2f7a20ce2d5d962c967a3e860
codesh85 <codesh85@gmail.com>
2024-11-02 22:53:28 +0800

    feat: util function for object

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,12 @@
 export function uuid(): string {
   return Math.random().toString()
 }
+
+export function isObject<T>(target: T): boolean {
+  return typeof target === 'object' && !Array.isArray(target) && target !== null;
+}
+
+export function hasProp<T>(target: T, key: string | number | symbol): boolean {
+  if (!isObject(target)) throw new Error('Value is not an object');
+  return Object.hasOwnProperty.call(target, key);
+}
