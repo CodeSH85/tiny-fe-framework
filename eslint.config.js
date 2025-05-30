d122dbcddb19bf2b1551f02268730d6fbd357dcd
CodeSH85 <codesh85@gmail.com>
2025-05-30 16:16:13 +0800

    chore: add stylelistic

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,34 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-
+import js from "@eslint/js"
+import globals from "globals"
+import stylistic from '@stylistic/eslint-plugin'
+import tseslint from "typescript-eslint"
+import { defineConfig } from "eslint/config"
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: {
+      js
+    },
+    extends: ["js/recommended"],
+    rules: {
+      "no-duplicate-imports": "error"
+    }
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
   tseslint.configs.recommended,
-]);
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      "semi": ['error', 'never'],
+      "comma-dangle": ["error", "never"]
+    }
+  }
+])
