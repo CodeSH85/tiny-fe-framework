554bcef7eb29b395e103561ced3cceca78455132
CodeSH85 <codesh85@gmail.com>
2025-05-30 22:04:06 +0800

    feat: add type checker helper functions

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,12 +1,24 @@
-export function uuid(): string {
-  return Math.random().toString()
+export function isObject<T>(target: T): boolean {
+  return typeof target === 'object' && !Array.isArray(target) && target !== null
 }
 
-export function isObject<T>(target: T): boolean {
-  return typeof target === 'object' && !Array.isArray(target) && target !== null;
+export function isArray<T>(target: T): boolean {
+  return Array.isArray(target)
+}
+
+export function isString<T>(target: T): boolean {
+  return typeof target === 'string'
+}
+
+export function isNumber<T>(target: T): boolean {
+  return typeof target === 'number'
+}
+
+export function isFunction<T>(target: T): boolean {
+  return typeof target === 'function'
 }
 
 export function hasProp<T>(target: T, key: string | number | symbol): boolean {
-  if (!isObject(target)) throw new Error('Value is not an object');
-  return Object.hasOwnProperty.call(target, key);
+  if (!isObject(target)) throw new Error('Value is not an object')
+  return Object.hasOwnProperty.call(target, key)
 }
